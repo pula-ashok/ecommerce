2e7c53d8152268b51e42d21e40fdabb8453962c5
pula-ashok <ashokpula0307@gmail.com>
2025-12-18 14:12:25 +0530

    adding user,address,user model

---
+++ ecommerce-be/src/main/java/com/pula/model/Address.java
@@ -0,0 +1,30 @@
+package com.pula.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String locality;
+    private String address;
+    private String city;
+    private String state;
+    private String pincode;
+    private String mobile;
+}
---
+++ ecommerce-be/src/main/java/com/pula/model/User.java
@@ -0,0 +1,39 @@
+package com.pula.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pula.domain.USER_ROLE;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private String email;
+    private String fullName;
+    private String mobile;
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+
+    @OneToMany
+    private Set<Address> addresses = new HashSet<>();
+    @ManyToMany
+    @JsonIgnore
+    private Set<Coupon> usedCoupons = new HashSet<>();
+
+}
